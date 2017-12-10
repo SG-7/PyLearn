0888e9be2050099173b473f9aec4558e5bae1d11
Chaos Beat <tmapps1993@gmail.com>
2017-12-09 21:23:15 -0600

    Added another example.

--- projects/io_example.py
+++ projects/io_example.py
@@ -1,5 +1,10 @@
-print("What is your name?")
-name = input()
+# Ask for the variable value from the user.
+name = input("What is your name?")
 print("Hello " + name + "!")
+# Changing the variable.
 name = "Chaos Beat"
 print("The programmer who made this was " + name + ".")
+
+firstName = input("What is your first name? ")
+lastName = input("What is your last name? ")
+print("Hello " + firstName + " " + lastName)
