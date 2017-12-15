d2c2f7967363937cea2d3ee3836d13ffd20b449f
Chaos Beat <tmapps1993@gmail.com>
2017-12-14 23:38:19 -0600

    Fixed most of the warnings in my code.

--- projects/pygame.py
+++ projects/pygame.py
@@ -5,12 +5,12 @@ import pygame
 pygame.init()
 
 # Colors can be defined afterwards.
-white = (255,255,255)
-black = (0,0,0)
-red = (255,0,0)
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
 
 # Here we set the display size and window name.
-gameDisplay = pygame.display.set_mode((800,600))
+gameDisplay = pygame.display.set_mode((800, 600))
 pygame.display.set_caption('Slither')
 
 # This is the variable for exiting the game.
@@ -26,10 +26,10 @@ clock = pygame.time.Clock()
 
 # Main game loop is next.
 while not gameExit:
-    for event in pygame.event.get(): # Event handling loop.
-        if event.type == pygame.QUIT: # Quitting the game.
+    for event in pygame.event.get():  # Event handling loop.
+        if event.type == pygame.QUIT:  # Quitting the game.
             gameExit = True
-        if event.type == pygame.KEYDOWN: # Movement
+        if event.type == pygame.KEYDOWN:  # Movement
             if event.key == pygame.K_LEFT:
                 lead_x_change = -10
             if event.key == pygame.K_RIGHT:
@@ -43,11 +43,11 @@ while not gameExit:
     lead_x += lead_x_change
     lead_y += lead_y_change
 
-    gameDisplay.fill(white) # Fills the window with the color white.
-    pygame.draw.rect(gameDisplay, black, [lead_x,lead_y,10,10]) # Adds a black square at the specified coordinates.
-    pygame.display.update() # Tells the display to update and show our graphics.
+    gameDisplay.fill(white)  # Fills the window with the color white.
+    pygame.draw.rect(gameDisplay, black, [lead_x, lead_y, 10, 10])  # Adds a black square at the specified coordinates.
+    pygame.display.update()  # Tells the display to update and show our graphics.
 
-    clock.tick(15) # Framerate set to 15fps.
+    clock.tick(15)  # Framerate set to 15fps.
 
-pygame.quit() # Exits PyGame.
-quit() # Exits Python and closes the window.
+pygame.quit()  # Exits PyGame.
+quit()  # Exits Python and closes the window.
